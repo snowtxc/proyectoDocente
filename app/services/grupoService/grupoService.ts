5650bc4917d93e6f3ed5b74312e5580d50662151
Rodrigo Castro <rcastro@whitemonkey.io>
2024-11-30 13:28:35 -0300

    changes

--- app/services/grupoService/grupoService.ts
+++ app/services/grupoService/grupoService.ts
@@ -6,10 +6,6 @@ import type { ListResponse } from '~/types/list-response';
 
 export const controller = "grupos";
 
-// src/services/authService.ts
-import type { CreateUserDTO, LoggedUser, LoginDTO } from '~/types/user'
-import { useNuxtApp } from '#app'
-import { defineStore } from 'pinia'
 export const grupoApi = {
   list: '/grupos/paged',
 }
@@ -22,7 +18,6 @@ export const useGrupoStore = defineStore('grupos', () => {
         loading.value = true;
         const { $request } = useNuxtApp()
         const resp = await $request('post', grupoApi.list, { page: 1, rowsPerPage: 100 })
-        console.log(resp);
         if (resp?.ok && resp?.data?.list) {
           grupos.value = resp.data.list;
         }
